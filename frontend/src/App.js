daee976d3544ddfbc9ca4d3871edeb49d701838f
DanVi007 <danvitrinh@outlook.com>
2023-11-07 17:14:33 +0100

    simple frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+    const [topicCount, setTopicCount] = useState(0);
+    const [topic, setTopic] = useState("");
+
+    const fetchTopicCountFromApi = async () => {
+        try {
+            const response = await axios.get("/topic_counter?topic=" + topic)
+            if (response.data !== null){
+                console.log(response.data)
+                setTopicCount(response.data)
+            } else {
+                console.error("Failed to fetch data from the API");
+            }
+        } catch(error) {
+            console.error("An error occurred while fetching data: ", error);
+        }
+        
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(topic !== "" && topic.length !== 0 && /\S/.test(topic)){
+            fetchTopicCountFromApi();
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label for="topic">Topic: </label>
+                <input type="text" id="topic" name="topic" value={topic} onChange={(e) => setTopic(e.target.value)} />
+            </form>
+            <p>Amount of times topic is mentioned in article: {topicCount}</p>
+        </div>
+    )
+}
+
+export default App;
